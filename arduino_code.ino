b939aaf5a4c97a0fcd1fa281fc92e2651f4fbcfa
Guru Srisha <gurusrisha1@gmail.com>
2024-10-09 23:09:44 +0530

    Update arduino_code.ino

--- arduino_code.ino
+++ arduino_code.ino
@@ -3,62 +3,91 @@
 Servo servoX;
 Servo servoY;
 Servo servoZ;
+Servo servoClamp;
 
-// Mid point angles, change as required for your servos
-int xDeg = 120;
-int yDeg = 25;
-int zDeg = 40;
+//Mid point values
+int xDeg=85;
+int yDeg=45;
+int zDeg=70;
+int clampDeg=40;
 
-void setup() {
-  pinMode(3, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
+void setup()
+{
+  pinMode(A0,INPUT);
+  pinMode(A1,INPUT);
+  pinMode(A2,INPUT);
+  pinMode(A3,INPUT);
+  pinMode(3,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(9,OUTPUT);
 
   servoX.attach(3);
   servoY.attach(5);
   servoZ.attach(6);
+  servoClamp.attach(9);
 
   servoX.write(xDeg);
   servoY.write(yDeg);
   servoZ.write(zDeg);
+  servoClamp.write(clampDeg);
 
   Serial.begin(9600);
 }
 
-void loop() {
-  if (Serial.available() > 0) {
-    char command = Serial.read();
+void loop()
+{
+  int leftJoyX=analogRead(A0);
+  int leftJoyY=analogRead(A3);
+  int rightJoyX=analogRead(A2);
+  int rightJoyY=analogRead(A1);
 
-    if (command == 'U') {
-      zDeg -= 15;
-    } else if (command == 'D') {
-      zDeg += 15;
-    } else if (command == 'F') {
-      yDeg += 15;
-    } else if (command == 'B') {
-      yDeg -= 15;
-    } else if (command == 'L') {
-      xDeg -= 25;
-    } else if (command == 'R') {
-      xDeg += 25;
-    }
+  delay(5);
 
-    // Minimum and Maximum angles, change as required for your servos
-    xDeg = min(150, max(0, xDeg));
-    yDeg = min(55, max(10, yDeg));
-    zDeg = min(100, max(40, zDeg));
+  if(leftJoyX<40) xDeg-=15;
+  else if(leftJoyX<200) xDeg-=10;
+  else if(leftJoyX>1004) xDeg+=15;
+  else if(leftJoyX>820) xDeg+=10;
+  
+  if(rightJoyY<40) yDeg-=12;
+  else if(rightJoyY<200) yDeg-=7;
+  else if(rightJoyY>1004) yDeg+=12;
+  else if(rightJoyY>820) yDeg+=7;
+  
+  if(rightJoyX<40) zDeg+=12;
+  else if(rightJoyX<200) zDeg+=7;
+  else if(rightJoyX>1004) zDeg-=12;
+  else if(rightJoyX>820) zDeg-=7;
+  
+  if(leftJoyY<250) clampDeg+=45;
+  else if(leftJoyY>800) clampDeg-=45;
 
-    servoX.write(xDeg);
-    servoY.write(yDeg);
-    servoZ.write(zDeg);
+  xDeg=min(150,max(0,xDeg));
+  yDeg=min(55,max(10,yDeg));
+  zDeg=min(100,max(40,zDeg));
+  clampDeg=min(180,max(40,clampDeg));
 
-    Serial.print("xDeg: ");
-    Serial.print(xDeg);
-    Serial.print(", yDeg: ");
-    Serial.print(yDeg);
-    Serial.print(", zDeg: ");
-    Serial.println(zDeg);
+  servoX.write(xDeg);
+  servoY.write(yDeg);
+  servoZ.write(zDeg);
+  servoClamp.write(clampDeg);
 
-    delay(50);
-  }
+  delay(50);
+
+  Serial.print("xDeg: ");
+  Serial.print(xDeg);
+  Serial.print("-");
+  Serial.print(leftJoyX);
+  Serial.print(", yDeg: ");
+  Serial.print(yDeg);
+  Serial.print("-");
+  Serial.print(rightJoyY);
+  Serial.print(", zDeg: ");
+  Serial.print(zDeg);
+  Serial.print("-");
+  Serial.print(rightJoyX);
+  Serial.print(", clampDeg: ");
+  Serial.print(clampDeg);
+  Serial.print("-");
+  Serial.println(leftJoyY);
 }
